1049995309629940b39589b7b880b6626e75de16
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2023-01-02 19:05:49 +0530

    Update Middle_Node.cpp

--- Middle_Node.cpp
+++ Middle_Node.cpp
@@ -17,3 +17,29 @@ public:
 
 // second solution 
 
+class Solution {
+    private:
+    int getLength(ListNode* head){
+        ListNode* temp = head;
+        int cnt =0;
+        while(temp!=NULL){
+            cnt++;
+            temp = temp -> next;
+        }
+        return cnt;
+    }
+public:
+    ListNode* middleNode(ListNode* head) {
+
+      int len = getLength(head);
+        int ans = len/2;
+        int cnt=0;
+        ListNode* temp = head;
+        
+        while(cnt < ans){
+            cnt++;
+            temp = temp->next;
+        }
+        return temp;
+    }
+};
