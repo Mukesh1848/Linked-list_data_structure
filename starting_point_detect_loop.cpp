b8626b76a6b8123fd1adce57614ebfaf9346657b
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2023-01-03 22:06:39 +0530

    Create starting_point_detect_loop.cpp

---
+++ starting_point_detect_loop.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        if (head == NULL || head -> next == NULL) return NULL;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (slow != NULL && fast != NULL){
+            fast = fast -> next;
+            
+            if (fast != NULL){
+                fast = fast -> next;
+            }
+            
+            slow = slow -> next;
+            
+            if (fast == slow) {
+                fast = fast;
+                slow = head;
+                
+                while (slow != fast){
+                    slow = slow -> next;
+                    fast = fast -> next;
+                }
+                return slow;
+            }
+        }
+        
+        return NULL;
+    }
+};
